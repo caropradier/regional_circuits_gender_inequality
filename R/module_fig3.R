7d7a7c3bbd1c0c6a175cac70604fd30d87151121
Carolina Pradier <carolinapradier@gmail.com>
2024-06-10 14:05:30 -0400

    first version of module 3

--- R/module_fig3.R
+++ R/module_fig3.R
@@ -1,21 +1,37 @@
 fig3_plot_server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    plot <- function(input_cut) {
-      d <- diamonds[sample(nrow(diamonds), 1000), ] %>%
-        filter(cut == input_cut)
+    plot <- function(dis) {
       
-      p <- ggplot(data = d, aes(x = carat, y = price)) +
-        geom_point(aes(text = paste("Clarity:", clarity))) +
-        geom_smooth(aes(colour = cut, fill = cut)) +
-        scale_color_manual(values = paleta_1) +
-        scale_fill_manual(values = paleta_1)
+      d <- results_list$plot_3_table %>% 
+        filter(level1 %in% dis)
+      
+      p <- ggplot(data = d,aes(x=women,y=latam,size=count,color = fct_reorder(level1,order), 
+                               text = paste0('</br>Representative terms: ',representation,
+                                             '</br><b>',level1,'</b>',
+                                             '</br>Women authorship: ',round(women*100,2),'%',
+                                             '</br>Published in Latin American journals or conferences: ',round(latam*100,2),'%')
+                               ))+
+        geom_point(alpha=0.5)+
+        geom_hline(yintercept = m_l)+
+        geom_vline(xintercept = m_w)+
+        scale_color_manual(values=mycolors)+
+        scale_size(range = c(2, 10))+
+        theme_minimal()+
+        theme(text = element_text(size = 12))+
+        scale_x_continuous(labels = function(x) paste0(x*100,"%"))+
+        scale_y_continuous(labels = function(x) paste0(x*100,"%"))+
+        labs(y="Published in Latin American journals or conferences",
+             x="Women authorship",
+             color="Discipline")+
+        guides(size = "none")+
+        theme(legend.position = "none")
       
       
-      ggplotly(p)
+      ggplotly(p, tooltip = "text")%>% layout(font = list(family = "Arial"))
     }
     
     output$plot <- renderPlotly({
-      plot(input$input_cut)
+      plot(input$input_discipline)
     })
   })
 }
@@ -26,14 +42,16 @@ fig3_plot_ui <- function(id) {
     title = "Figure 3",
     sidebarLayout(
       sidebarPanel(
-        selectInput(ns("input_cut"),
-                    label = "select cut",
-                    choices = diamonds$cut %>% unique(),
-                    selected = "Premium",
-                    multiple = FALSE
+        selectInput(ns("input_discipline"),
+                    label = "Select discipline",
+                    choices = unique(results_list$plot_3_table$level1),
+                    selected = unique(results_list$plot_3_table$level1),
+                    multiple = TRUE
         )
-      ),
-      mainPanel(plotlyOutput(ns("plot")))
+        , width = 3),
+      mainPanel(h2("Topic space"),
+        plotlyOutput(ns("plot"), height = 800)%>% withSpinner(type = 5, color ="black")
+                )
     )
   )
 }
